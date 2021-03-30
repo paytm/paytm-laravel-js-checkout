1d404fc1ca40f9e46da64f6fdbfd37bd5376159d
rahul bisht <rahul2.bisht@paytm.com>
2021-03-30 09:13:05 +0530

    intial commit

---
+++ src/PaytmWalletManager.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Paytm\JsCheckout;
+
+use Illuminate\Support\Manager;
+use Illuminate\Http\Request;
+class PaytmWalletManager extends Manager implements Contracts\Factory{
+	
+
+	protected $config;
+
+
+
+	public function with($driver){
+		return $this->driver($driver);
+	}
+
+	protected function createReceiveDriver(){
+		$this->config = $this->app['config']['services.paytm-wallet'];
+
+		return $this->buildProvider(
+			'Paytm\JsCheckout\Providers\ReceivePaymentProvider',
+			$this->config
+			);
+	}
+
+	protected function createStatusDriver(){
+		$this->config = $this->app['config']['services.paytm-wallet'];
+		return $this->buildProvider(
+			'Paytm\JsCheckout\Providers\StatusCheckProvider',
+			$this->config
+			);
+	}
+
+	protected function createBalanceDriver(){
+		$this->config = $this->app['config']['services.paytm-wallet'];
+		return $this->buildProvider(
+			'Paytm\JsCheckout\Providers\BalanceCheckProvider',
+			$this->config
+			);
+	}
+
+	protected function createAppDriver(){
+		$this->config = $this->app['config']['services.paytm-wallet'];
+		return $this->buildProvider(
+			'Paytm\JsCheckout\Providers\PaytmAppProvider',
+			$this->config
+			);
+	}
+
+	protected function createRefundDriver() {
+		$this->config = $this->app['config']['services.paytm-wallet'];
+		return $this->buildProvider(
+			'Paytm\JsCheckout\Providers\RefundPaymentProvider',
+			$this->config
+			);
+	}
+	
+	protected function createRefundStatusDriver(){
+		$this->config = $this->app['config']['services.paytm-wallet'];
+		return $this->buildProvider(
+			'Paytm\JsCheckout\Providers\RefundStatusCheckProvider',
+			$this->config
+			);
+	}
+	
+	public function getDefaultDriver(){
+		throw new \Exception('No driver was specified. - Laravel Paytm Wallet');
+	}
+
+	public function buildProvider($provider, $config){
+		return new $provider(
+			$this->app['request'],
+			$config
+			);
+	}
+}
