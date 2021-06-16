0709405a185008697c6e7ab501f375e9866a41af
Abhishek Awasthi <74229697+abpaytm@users.noreply.github.com>
2021-06-16 15:01:50 +0530

    Update PaytmConstants.php

--- lib/PaytmConstants.php
+++ lib/PaytmConstants.php
@@ -1,44 +1,44 @@
 <?php
 
 class PaytmConstants{
-	CONST TRANSACTION_STATUS_URL_PRODUCTION		= "https://securegw.paytm.in/order/status";
-	CONST TRANSACTION_STATUS_URL_STAGING		= "https://securegw-stage.paytm.in/order/status";
+	CONST TRANSACTION_STATUS_URL_PRODUCTION		                = "https://securegw.paytm.in/order/status";
+	CONST TRANSACTION_STATUS_URL_STAGING		                = "https://securegw-stage.paytm.in/order/status";
 
 	CONST PRODUCTION_HOST						= "https://securegw.paytm.in/";
-	CONST STAGING_HOST							= "https://securegw-stage.paytm.in/";
+	CONST STAGING_HOST					        = "https://securegw-stage.paytm.in/";
 
 	CONST ORDER_PROCESS_URL						= "order/process";
 	CONST ORDER_STATUS_URL						= "order/status";
-	CONST INITIATE_TRANSACTION_URL				= "theia/api/v1/initiateTransaction";
+	CONST INITIATE_TRANSACTION_URL				        = "theia/api/v1/initiateTransaction";
 	CONST CHECKOUT_JS_URL						= "merchantpgpui/checkoutjs/merchants/MID.js";
 
 
 	CONST SAVE_PAYTM_RESPONSE 					= true;
-	CONST CHANNEL_ID							= "WEB";
+	CONST CHANNEL_ID						= "WEB";
 	CONST APPEND_TIMESTAMP						= true;
-	CONST X_REQUEST_ID							= "PLUGIN_WOOCOMMERCE_";
+	CONST X_REQUEST_ID						= "PLUGIN_LARAVEL_";
 	CONST PLUGIN_DOC_URL						= "https://developer.paytm.com/docs/eCommerce-plugin/woocommerce/";
 
 	CONST MAX_RETRY_COUNT						= 3;
 	CONST CONNECT_TIMEOUT						= 10;
-	CONST TIMEOUT								= 10;
+	CONST TIMEOUT							= 10;
 
-	CONST LAST_UPDATED							= "20191005";
-	CONST PLUGIN_VERSION						= "2.0";
+	CONST LAST_UPDATED						= "20210616";
+	CONST PLUGIN_VERSION						= "2.5";
 
 	CONST CUSTOM_CALLBACK_URL					= "http://localhost/platform/wordpress/woocommerce/";
 
 
-	CONST ID									= "paytm";
-	CONST METHOD_TITLE							= "Paytm Payments";
+	CONST ID						        = "paytm";
+	CONST METHOD_TITLE						= "Paytm Payments";
 	CONST METHOD_DESCRIPTION					= "The best payment gateway provider in India for e-payment through credit card, debit card & netbanking.";
 
-	CONST TITLE									= "Paytm";
-	CONST DESCRIPTION							= "The best payment gateway provider in India for e-payment through credit card, debit card & netbanking.";
+	CONST TITLE							= "Paytm";
+	CONST DESCRIPTION						= "The best payment gateway provider in India for e-payment through credit card, debit card & netbanking.";
 
 	 
 	
-	CONST FRONT_MESSAGE						= "Thank you for your order, please click the button below to pay with paytm.";
+	CONST FRONT_MESSAGE					= "Thank you for your order, please click the button below to pay with paytm.";
 	CONST NOT_FOUND_TXN_URL					= "Something went wrong. Kindly contact with us.";
 	CONST PAYTM_PAY_BUTTON					= "Pay via Paytm";
 	CONST CANCEL_ORDER_BUTTON				= "Cancel order & Restore cart";
@@ -53,14 +53,14 @@ class PaytmConstants{
 	//Success
 	CONST SUCCESS_ORDER_MESSAGE				= "Thank you for your order. Your payment has been successfully received.";
 	CONST RESPONSE_SUCCESS					= "Updated <b>STATUS</b> has been fetched";
-	CONST RESPONSE_STATUS_SUCCESS			= " and Transaction Status has been updated <b>PENDING</b> to <b>%s</b>";
+	CONST RESPONSE_STATUS_SUCCESS			        = " and Transaction Status has been updated <b>PENDING</b> to <b>%s</b>";
 	CONST RESPONSE_ERROR					= "Something went wrong. Please again'";
 
 	//Error
 	CONST PENDING_ORDER_MESSAGE				= "Your payment has been pending!";
 	CONST ERROR_ORDER_MESSAGE				= "Your payment has been failed!";
-	CONST ERROR_SERVER_COMMUNICATION		= "It seems some issue in server to server communication. Kindly connect with us.";
-	CONST ERROR_CHECKSUM_MISMATCH			= "Security Error. Checksum Mismatched!";
+	CONST ERROR_SERVER_COMMUNICATION		        = "It seems some issue in server to server communication. Kindly connect with us.";
+	CONST ERROR_CHECKSUM_MISMATCH			        = "Security Error. Checksum Mismatched!";
 	CONST ERROR_AMOUNT_MISMATCH				= "Security Error. Amount Mismatched!";
 	CONST ERROR_INVALID_ORDER				= "No order found to process. Kindly contact with us.";
 	CONST ERROR_CURL_DISABLED				= "cURL is not enabled properly. Please verify.";
